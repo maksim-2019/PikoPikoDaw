7117d79ffe53a6d02b1be68586c0aa6371910f11
AidaMG <aida3montes@gmail.com>
2020-02-26 13:56:50 +0100

    creación de los atributos, constructor y métodos de turnoJugador

--- src/main/java/com/mycompany/pikopiko/TurnoJugadores.java
+++ src/main/java/com/mycompany/pikopiko/TurnoJugadores.java
@@ -12,7 +12,31 @@ import java.util.ArrayList;
  * @author aida
  */
 public class TurnoJugadores {
+    // atributos de clase de TurnoJugadores
+    ArrayList<Jugador> jugador;
+    int turno;
+    
+    //Constructor con la lista
 
+    public TurnoJugadores(ArrayList<Jugador> jugador) {
+        this.jugador = jugador;
+    }
+    
+    // getTurnoJugador : para saber quien le toca
+    public int getTurnoJugador() {
+        return 0;
+        
+    }
+    // getTodosTurno: para saber cual es el turno 
+    public int getTodosTurno() {
+        return 0;
+        
+    }
+    
+    // Método pasarSiguiente
+    public void pasarSiguiente(){
+    
+    }
     
     
 }
