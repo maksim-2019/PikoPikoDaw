4dc42ef586d8f4eea212e6b3bb6c40a590dc2bae
AidaMG <aida3montes@gmail.com>
2020-02-28 14:56:40 +0100

    Añadimos toString en la clase Jugador para probar la clase TurnoJugadores

--- src/main/java/com/mycompany/pikopiko/Jugador.java
+++ src/main/java/com/mycompany/pikopiko/Jugador.java
@@ -27,5 +27,10 @@ public class Jugador {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    @Override
+    public String toString() {
+        return "Jugador{" + "nombre=" + nombre + '}';
+    }
     
 }
