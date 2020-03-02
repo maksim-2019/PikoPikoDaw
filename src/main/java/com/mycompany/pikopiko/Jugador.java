b3b14e7efcbf5f31db6b490b4c7dd46c195493d9
AlexC <alexcuevasfer@gmail.com>
2020-03-02 12:54:18 +0100

    Añade atributos, metodos, y main a Jugador

--- src/main/java/com/mycompany/pikopiko/Jugador.java
+++ src/main/java/com/mycompany/pikopiko/Jugador.java
@@ -5,20 +5,36 @@
  */
 package com.mycompany.pikopiko;
 
+import java.util.Random;
+
 /**
  *
  * @author jose
  */
 public class Jugador {
-    
+
     //Atributos
     private String nombre;
-    
+    private int puntosAcumulados;
+    private Dado[] arrayDado;
+
+    private Random r = new Random();
+
     //Constructor
     public Jugador(String nombre) {
         this.nombre = nombre;
+        this.puntosAcumulados = puntosAcumulados;
+        this.arrayDado = arrayDado;
     }
-    
+
+    //Metodos
+    public void tirarDados() {
+
+        arrayDado[0].mostrarDado();
+
+        
+    }
+
     //Getters
     public String getNombre() {
         return nombre;
@@ -28,9 +44,25 @@ public class Jugador {
         this.nombre = nombre;
     }
 
+    public int getPuntosAcumulados() {
+        return puntosAcumulados;
+    }
+
+//    public void setPuntosAcumulados(int puntosAcumulados) {
+//        this.puntosAcumulados = puntosAcumulados;
+//    }
     @Override
     public String toString() {
         return "Jugador{" + "nombre=" + nombre + '}';
     }
     
+    public static void main(String[] args) {
+        Jugador p = new Jugador("e");
+        p.tirarDados();
+        
+        for (int i = 0; i < p.arrayDado.length; i++) {
+            System.out.println(p.arrayDado[i]);
+        }
+ 
+    }
 }
