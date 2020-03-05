f2a35075b445a71f3b0c5e5ab7d85178b5d5ac46
AlexC <alexcuevasfer@gmail.com>
2020-03-05 12:35:44 +0100

    Añade métodos a la clase PilaRacion

--- src/main/java/com/mycompany/pikopiko/PilaRacion.java
+++ src/main/java/com/mycompany/pikopiko/PilaRacion.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  * @author alex
  */
 public class PilaRacion {
-    
+
     // Declaración del array y del tope de la pila
     ArrayList<Racion> arrayRacion = new ArrayList<>();
     private int tope;
@@ -22,5 +22,38 @@ public class PilaRacion {
         this.tope = 16; // Hay 16 fichas en total
     }
 
+    // Inserta una ración a la pila del jugador
+    public void push(Racion racion1) {
+        if (arrayRacion.size() <= (this.tope - 1)) {
+            this.arrayRacion.add(racion1);
+        } else {
+            System.out.println("esta llena");
+        }
+    }
+
+    // Saca una ración de la pila
+    public Racion pop() {
+        if (!estaVacia()) {
+            return this.arrayRacion.remove(arrayRacion.size() - 1);
+        }
+        return null;
+    }
+
+    // Devuelve un booleano que indica si hay Raciones o no
+    public boolean estaVacia() {
+        return this.arrayRacion.isEmpty();
+    }
+
+    // Muestra cuantas Raciones tiene el jugador independientemente de la puntuacion
+    public int cantidadRaciones() {
+        return arrayRacion.size();
+    }
+
+    // Muestra las Raciones que tiene el jugador
+    public void mostrarPilaRacion() {
+        for (Racion aux : this.arrayRacion) {
+            System.out.print(aux + " - ");
+        }
+    }
 
 }
